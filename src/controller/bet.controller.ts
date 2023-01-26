a2324fc791021943dc43dfca334e3b3a51387f21
Federico di Donato <federicodido525@gmail.com>
2023-01-26 09:39:20 -0800

    query param

--- src/controller/bet.controller.ts
+++ src/controller/bet.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Path, Post, Route } from 'tsoa';
+import { Controller, Get, Path, Post, Route, Query } from 'tsoa';
 import betService from '../services/bet.service';
 
 @Route('bet')
 export class BetController extends Controller {
   @Get()
-  public async getBets() {
+  public async getBets(@Query() options: any) {
     const requests = await betService.getBets(null);
     return requests;
   }
--- src/controller/pro_report.controller.ts
+++ src/controller/pro_report.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Path, Post, Route } from 'tsoa';
+import { Controller, Get, Path, Post, Route, Query } from 'tsoa';
 import proReportService from '../services/pro_report.service';
 
 @Route('pro_report')
 export class ProReportController extends Controller {
   @Get()
-  public async getProReports() {
+  public async getProReports(@Query() options: any) {
     const requests = await proReportService.getReports(null);
     return requests;
   }
