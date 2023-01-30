258c28f36aa118dc31c11bf55e94d99f205958b8
Francisco Molina <frankmolinadev@gmail.com>
2023-01-30 09:51:02 -0300

    changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: '127.0.0.1',
   port: 3306,
   username: 'root',
-  password: '',
+  password: '265813Fm.',
   database: 'sports',
   synchronize: true,
   logging: false,
