6724bedd0f882c326a3dbc021b986ecaac486a12
Francisco Molina <frankmolinadev@gmail.com>
2023-01-31 15:53:58 -0300

    lol

--- src/scraping/index.ts
+++ src/scraping/index.ts
@@ -1,23 +1,23 @@
-import NFL from "./odd/nfl"
-import NCAAF from "./odd/ncaaf";
-import NBA from "./odd/nba";
-import NCAAB from "./odd/ncaab";
-import NHL from "./odd/nhl";
-import SOCCER from "./odd/soccer";
-import WNBA from "./odd/wnba";
-import UFC from "./odd/ufc";
-import NASCAR from "./odd/nascar";
-import ATP from "./odd/atp";
-import WTA from "./odd/wta";
+import NFL from './odd/nfl';
+import NCAAF from './odd/ncaaf';
+import NBA from './odd/nba';
+import NCAAB from './odd/ncaab';
+import NHL from './odd/nhl';
+import SOCCER from './odd/soccer';
+import WNBA from './odd/wnba';
+import UFC from './odd/ufc';
+import NASCAR from './odd/nascar';
+import ATP from './odd/atp';
+import WTA from './odd/wta';
 import ProReportNFL from './pro_report/nfl';
-import ProReportNBA from './pro_report/nba'
-import ProReportNCAAB from './pro_report/ncaab'
-import ProReportNHL from './pro_report/nhl'
-import { sleep } from "../utils/timeout";
+import ProReportNBA from './pro_report/nba';
+import ProReportNCAAB from './pro_report/ncaab';
+import ProReportNHL from './pro_report/nhl';
+import { sleep } from '../utils/timeout';
 
-import Scraper from "./scraper";
+import Scraper from './scraper';
 
-const nfl= new NFL();
+const nfl = new NFL();
 const ncaaf = new NCAAF();
 const nba = new NBA();
 const ncaab = new NCAAB();
@@ -37,31 +37,35 @@ const proReportNhl = new ProReportNHL();
 const scraper = new Scraper();
 
 export async function startScraping() {
-    // while (true) {
-    //     try {
-    //         console.log('------------------------------------- scraping started -------------------------------------')
-    //         await nfl.start();
-    //         await ncaaf.start();
-    //         await nba.start();
-    //         await ncaab.start();
-    //         await nhl.start();
-            // await soccer.start();
-    //         await wnba.start();
-    //         await ufc.start();
-    //         await nascar.start();
-    //         await atp.start();
-    //         await wta.start();
-    
-    //         await proReportNfl.start();
-    //         await proReportNba.start();
-    //         await proReportNcaab.start();
-    //         await proReportNhl.start();
-    //         console.log('------------------------------------- scraping ended -------------------------------------')
-    //     } catch (error) {
-    //         console.log('error = ', error)
-    //     }
+  while (true) {
+    try {
+      console.log(
+        '------------------------------------- scraping started -------------------------------------',
+      );
+      await nfl.start();
+      await ncaaf.start();
+      await nba.start();
+      await ncaab.start();
+      await nhl.start();
+      await soccer.start();
+      await wnba.start();
+      await ufc.start();
+      await nascar.start();
+      await atp.start();
+      await wta.start();
 
-    //     await sleep(600000) // delay for 10 minutes
-    // }
-    await scraper.start();
+      await proReportNfl.start();
+      await proReportNba.start();
+      await proReportNcaab.start();
+      await proReportNhl.start();
+      console.log(
+        '------------------------------------- scraping ended -------------------------------------',
+      );
+    } catch (error) {
+      console.log('error = ', error);
+    }
+
+    await sleep(600000); // delay for 10 minutes
+  }
+  await scraper.start();
 }
